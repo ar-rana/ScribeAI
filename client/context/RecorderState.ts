a03dd227b5a9b7b08fd4aee1a15cd9d2a351fe02
Aryan Rana <aryan.rana2005@gmail.com>
2025-11-22 19:10:17 +0530

    added functionality for recording audio

--- client/context/RecorderState.ts
+++ client/context/RecorderState.ts
@@ -2,10 +2,11 @@ import { nullable } from "better-auth";
 import { assign, createActor, createMachine } from "xstate";
 
 interface RecorderContext {
-  mediaStream: MediaStream | null;
   recorder: MediaRecorder | null;
   transcript: string;
   type: "mic" | "tab";
+  audio: Blob[];
+  audioURL: string | null;
 }
 
 type RecorderEvents =
@@ -24,10 +25,11 @@ export const recorderState = createMachine(
       events: {} as RecorderEvents,
     },
     context: {
-      mediaStream: null,
       recorder: null,
       transcript: "",
       type: "mic",
+      audio: [],
+      audioURL: "",
     },
     initial: "idle",
     on: {
@@ -65,16 +67,48 @@ export const recorderState = createMachine(
   },
   {
     actions: {
-      startRecording: () => {
-        console.log("▶ Recording started");
+      startRecording: (context) => {
+        if (context.context.recorder) {
+          context.context.recorder?.resume();
+          console.log("Audio Resumed");
+          return context;
+        }
+        console.log("Recording started");
+        const getStream = async () => {
+          return context.context.type === "mic"
+            ? navigator.mediaDevices.getUserMedia({ audio: true })
+            : navigator.mediaDevices.getDisplayMedia({
+                audio: true,
+                video: false,
+              });
+        };
+
+        getStream()?.then((stream: MediaStream) => {
+          const recorder = new MediaRecorder(stream);
+          const audioBlobs: Blob[] = [];
+
+          recorder.ondataavailable = (e) => {
+            if (e.data.size > 0) audioBlobs.push(e.data);
+          };
+
+          recorder.onstop = () => {
+            const blob = new Blob(audioBlobs, { type: "audio/webm" }); // combine all chunks into one audio BLOB
+            const url = URL.createObjectURL(blob); // this URL used for playback
+
+            context.context.audio = audioBlobs;
+            context.context.audioURL = url;
+          };
+        });
       },
 
-      stopRecording: () => {
+      stopRecording: ({ context }) => {
+        context.recorder?.stop();
         console.log("■ Recording stopped");
       },
 
-      pauseRecording: () => {
-        console.log("|| Recording Paused");
+      pauseRecording: ({ context }) => {
+        context.recorder?.pause();
+        console.log("Recording Paused");
       },
 
       setType: assign({
