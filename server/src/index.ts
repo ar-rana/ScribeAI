40bdd492d24cd5c2793d5dc3e0185fb7f8486340
Aryan Rana <aryan.rana2005@gmail.com>
2025-11-23 23:56:42 +0530

    completed server routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import SocketService from "./sockets/recording.js";
 import z from 'zod';
 import prismaClient from './services/prisma.js';
+import { generateSummary } from './services/geminiAI.js';
 
 const app = express();
 const port = 3333;
@@ -43,27 +44,70 @@ app.post("/user/login", async (req, res) => {
   }
 });
 
-app.post("/transcript", async (req, res) => {
+app.post("/user/recording", async (req, res) => {
   try {
-    const zdata = transcriptPayload.parse(req.body);
-    const user = await prismaClient.transcripts.find({
+    const zdata = recording.parse(req.body);
+    const recordingData = await prismaClient.transcripts.create({
       data: {
-        name : zdata.name,
-        email : zdata.email
+        userEmail: zdata.email,
+        transcript: zdata.transcript,
+        title: zdata.title,
+        duration: zdata.duration,
+        client_audio_id: zdata.client_audio_id
       },
     });
-    res.status(200).json({ message: "User saved", user: user.email });
+    res.status(200).json({ message: `Recording Saved ${recordingData.id}` });
   } catch (err) {
     const error = err as Error;
     res.status(400).json({ error: error.message || "Invalid Input" });
   }
 });
 
-app.get("/summary/:id", async (req, res) => {
-  const { id } = req.params;
+app.post("/transcript", async (req, res) => {
   try {
-    console.log("received");
-    res.status(200).json({ message: `Hello ${id}` });
+    const zdata = transcriptPayload.parse(req.body);
+    const transcript = await prismaClient.transcripts.findFirst({
+      where: {
+        userEmail: zdata.email,
+        client_audio_id: zdata.client_id
+      },
+    });
+    res.status(200).json({ transcript: transcript });
+  } catch (err) {
+    const error = err as Error;
+    res.status(400).json({ error: error.message || "Invalid Input" });
+  }
+});
+
+app.post("/summary", async (req, res) => {
+  try {
+    const zdata = transcriptPayload.parse(req.body);
+    const transcript = await prismaClient.transcripts.findFirst({
+      where: {
+        userEmail: zdata.email,
+        client_audio_id: zdata.client_id
+      },
+    });
+
+    let response: string = "";
+    if (transcript) response = await generateSummary(transcript.transcript);
+    console.log("summary: ", response);
+    res.status(200).json({ summary: response });
+  } catch (err) {
+    const error = err as Error;
+    res.status(400).json({ error: error.message || "Invalid Input" });
+  }
+});
+
+app.get("/list/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const transcripts = await prismaClient.transcripts.findMany({
+      where: {
+        userEmail: email,
+      },
+    });
+    res.status(200).json({ recordings: transcripts });
   } catch (err) {
     const error = err as Error;
     res.status(400).json({ error: error.message || "Invalid Input" });
@@ -94,6 +138,14 @@ const loginPayloadSchema = z.object({
 
 const transcriptPayload = z.object({
   email: z.email(),
-  name: z.string(),
+  client_id: z.string(),
+});
+
+const recording = z.object({
+  email: z.email(),
+  transcript: z.string(),
+  title: z.string().optional(),
+  duration: z.string(),
+  client_audio_id: z.string()
 });
 
--- server/src/services/geminiAI.ts
+++ server/src/services/geminiAI.ts
@@ -12,3 +12,16 @@ const googleAI = new GoogleGenerativeAI(gemini_api_key);
 export const geminiModel = googleAI.getGenerativeModel({
   model: "gemini-2.5-flash",
 });
+
+export const generateSummary = async (transcript: string): Promise<string> => {
+  try {
+    const instructions = "You have to Generate the Short Summary of a Audio Transcript. ONLY return the summary for :- ";
+    const result = await geminiModel.generateContent(instructions + transcript);
+    const response = result.response;
+
+    return response.text();
+  } catch (error) {
+    console.log("GEMINI response error", error);
+  }
+  return "";
+}
